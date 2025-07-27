1da5791a545df69e1ff3cb732a7b9b6a1a855cd9
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-07-26 21:05:03 -0300

    aula001 modulo 01

---
+++ curso_udemy/modulo01/aula001.py
@@ -0,0 +1,14 @@
+#Comentario em Python
+print(123)
+print('Hello, Word!')
+
+"""
+DocString
+Comentario
+de multiplas
+linhas
+"""
+
+'''
+pode ser com asoas simples também
+'''
---
+++ curso_udemy/modulo01/aula002.py
@@ -0,0 +1,6 @@
+print(12, 34, sep='-')#separador dos elementos
+print(56, 78, 45, sep=' > ', end='\r\n')
+print(56, 78, end='\n##')
+print(56, 78)
+
+#\n\r quebras de linhas = CRLF
