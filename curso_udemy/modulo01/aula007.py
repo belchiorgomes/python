6a6c1b85aeb5698df2d6d722ed928b5e7bb53871
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-07-29 08:33:02 -0300

    aula 006 até aula008

---
+++ curso_udemy/modulo01/aula007.py
@@ -0,0 +1,22 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+# nome_completo = 'Luiz Otávio Miranda'
+# soma_dois_mais_dois = 2 + 2
+# int_um = bool('1')
+# print(int_um, type(int_um))
+# print(nome_completo, soma_dois_mais_dois)
+
+# nome_completo = 'Breno Belchior de Freitas Gomes'
+# soma_dois_mais_dois = 2 + 2
+# print(f'Seu nome completo e {nome_completo} \n e a soma entre os numeros é {soma_dois_mais_dois}')
+
+nome = 'Raquel'
+idade = 30
+maior_idade = idade >= 18
+print('Nome:', nome, 'Idade', idade)
+print('É maior?', maior_idade)
---
+++ curso_udemy/modulo01/aula008.py
@@ -0,0 +1,14 @@
+nome = 'Gabriela'
+sobrenome = 'Silva'
+idade = 31
+ano_nascimento = 2025 - idade
+maior_idade = idade >= 18
+
+
+
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_idade)
