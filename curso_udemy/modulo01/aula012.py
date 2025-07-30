d869eb7c194844885d368a028d3c2ea6672fe9de
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-07-30 08:32:48 -0300

    estudo de python aula 09 ate aula 013

---
+++ curso_udemy/modulo01/aula012.py
@@ -0,0 +1,7 @@
+nome = 'Breno Belchior de Freitas Gomes'
+altura = 1.72
+peso = 63
+
+imc = peso / (altura * altura)
+
+print(f'A/O paciente {nome} com a altura {altura:.2f} e com o peso {peso} tem um IMC de {imc:.2f}')
