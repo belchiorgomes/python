c62955c744d6c54f322e1088a1c1d233401f449f
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-08-27 10:18:16 -0300

    exercicio python sobre cpf

---
+++ curso_udemy/modulo01/aula051.py
@@ -0,0 +1,11 @@
+# print('Valor' if True else 'Outro valor')
+
+condicao = 10 == 11
+variavel = 'Valor' if condicao else 'Outro valor'
+print(variavel)
+
+print('---------------------')
+
+digito = 6
+novo_digito = digito if digito <= 9 else 0
+print(novo_digito)
