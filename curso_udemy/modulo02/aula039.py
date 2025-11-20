4af4e62fcbbbd12bebb6857aff7a4f9d635289ba
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-11-20 10:52:51 -0300

    aula de python

---
+++ curso_udemy/modulo02/aula039.py
@@ -0,0 +1,26 @@
+# def fora(x):
+#     a = x
+
+#     def dentro():
+#         return a
+#     return dentro
+
+# dentro = fora(10)
+
+# print(dentro())
+
+# print(globals())
+
+def concatenar(string_inicial):
+    valor_final = string_inicial
+
+    def interna(valor_a_concatenar):
+        nonlocal valor_final
+        valor_final += valor_a_concatenar
+        return valor_final
+    return interna
+
+c = concatenar('a')
+print(c('b'))
+print(c('c'))
+print(c('d'))
